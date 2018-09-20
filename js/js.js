bf0a591db8dab5f8c935c0a2afec4a3b88b59dfa
Unknown <99053978@mydavinci.nl>
2018-09-20 22:24:53 +0200

    WIP

---
+++ js/js.js
@@ -0,0 +1,9 @@
+var failCounter = 0;
+
+if (prompt("Who started the MCU?") === "Iron Man") {
+	if (prompt("Who plays the character of blackwidow?") === "Scarlet Johanson") {
+
+	}
+}else if (false) {
+	document.write(++failCounter);
+}
