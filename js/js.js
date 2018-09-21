5db88bf97cf21ee4bee20a972e13b0d8f9c45928
Unknown <99053978@mydavinci.nl>
2018-09-21 14:26:18 +0200

    WIP

--- js/js.js
+++ js/js.js
@@ -1,25 +1,5 @@
 var failCounter = 0;
 
-/*if (prompt("q") == "ja") {
-	if (prompt("q2") == "ja") {
-		if (prompt("q3") == "ja") {
-			if (prompt("q4") == "ja") {
-				if (prompt("q5") == "ja") {
-					if (prompt("q6") == "ja") {
-
-					}else{console.log(failCounter++)}
-				}else{console.log(failCounter++)}
-			}else{console.log(failCounter++)}
-		}else{console.log(failCounter++)}
-	}else{console.log(failCounter++)}
-}else{console.log(failCounter++)}
-
-
-for (failCounter = 0; failCounter == 3; failCounter++) {
-    if (failCounter === 3) { break; }
-    text += "The number is " + failCounter + "<br>";
-} */
-
 if (prompt("q") != "ja") {
 	addFailure();
 }
@@ -38,7 +18,6 @@ if (prompt("q4") != "ja") {
 if (prompt("q5") != "ja") {
 	addFailure();
 }
-
 
 
 function addFailure(){
