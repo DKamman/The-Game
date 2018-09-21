58737b7c39818e3129a34a74e6e9c8ad16696d91
Unknown <99053978@mydavinci.nl>
2018-09-21 13:55:06 +0200

    WIP

--- js/js.js
+++ js/js.js
@@ -1,19 +1,50 @@
 var failCounter = 0;
 
-if (failCounter == 3) {
-	document.write("GAME OVER, you made too many mistakes!")
+/*if (prompt("q") == "ja") {
+	if (prompt("q2") == "ja") {
+		if (prompt("q3") == "ja") {
+			if (prompt("q4") == "ja") {
+				if (prompt("q5") == "ja") {
+					if (prompt("q6") == "ja") {
+
+					}else{console.log(failCounter++)}
+				}else{console.log(failCounter++)}
+			}else{console.log(failCounter++)}
+		}else{console.log(failCounter++)}
+	}else{console.log(failCounter++)}
+}else{console.log(failCounter++)}
+
+
+for (failCounter = 0; failCounter == 3; failCounter++) {
+    if (failCounter === 3) { break; }
+    text += "The number is " + failCounter + "<br>";
+} */
+
+if (prompt("q") != "ja") {
+	addFailure();
+}
+if (prompt("q2") != "ja") {
+	addFailure();
+}
+if (prompt("q2") != "ja") {
+	addFailure();
+}
+if (prompt("q3") != "ja") {
+	addFailure();
+}
+if (prompt("q4") != "ja") {
+	addFailure();
+}
+if (prompt("q5") != "ja") {
+	addFailure();
 }
 
-if (prompt("Who started the MCU?") === "Iron Man") {
-	}else(document.write(++failCounter));{
 
-}if (prompt("Who plays the character of blackwidow?") === "Scarlet Johanson") {
-	}else(document.write(++failCounter));{
 
-}if (prompt("Who plays the character of blackwidow?") === "Scarlet Johanson") {
-	}else(document.write(++failCounter));{
-
-}if (prompt("Who plays the character of blackwidow?") === "Scarlet Johanson") {
-	}else(document.write(++failCounter));{
-
+function addFailure(){
+	console.log(failCounter++);
+	if (failCounter == 3) {
+		alert("loser");
+		throw new Error("Game Over!");
+	}
 }
